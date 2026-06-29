1bdc1793307a54ed19d060b26a0a4b497978c3b7
Yash-Yelave <rostopedia@gmail.com>
2026-06-29 22:06:33 +0530

    feat: configure alembic environment for async database migrations

--- report-management-backend/alembic/env.py
+++ report-management-backend/alembic/env.py
@@ -15,9 +15,8 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from app.models.base import Base
-# target_metadata = Base.metadata
-target_metadata = None
+from app.models import Base
+target_metadata = Base.metadata
 
 config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
